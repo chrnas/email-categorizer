0db3731c843f15444252d4cabd1c49992eb5cfba
selintaskin <73575125+selintaskin@users.noreply.github.com>
2024-11-20 14:48:28 +0000

    Update statistics_collector.py

--- email_categorizer/src/observing/statistics_collector.py
+++ email_categorizer/src/observing/statistics_collector.py
@@ -11,6 +11,14 @@ class StatCollector(Observer):
             return  # Ignore irrelevant events
         # Handle relevant event
         # Get statistics from the context
+        for email_class, met in statistics.items():
+            if email_class in data.target_names:  
+                print(f"Email Type: {email_class}")
+                print(f"Precision: {met['precision']}")
+                print(f"Recall: {met['recall']}")
+                print(f"F1-Score: {met['f1-score']}")
+                print(f"Support: {met['support']}")
+                print("\n")
         self.statistics = statistics
         self.display()
 
