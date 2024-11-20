6670abda95671af7d47b9918a0086e4b3ffeb14f
Patrick Vorreiter <patrick.vorreiter@gmx.de>
2024-11-20 10:42:04 +0000

    Same stuff

--- email_categorizer/src/observing/result_displayer.py
+++ email_categorizer/src/observing/result_displayer.py
@@ -1,8 +1,9 @@
+from models.base import BaseModel
 from .observe import Observer
 from context_classification.context import ContextClassifier
 
-class StatCollector(Observer):
+class ResultDisplayer(Observer):
     
     def update(self, context: ContextClassifier):
         context.print_results()
-        
+    
